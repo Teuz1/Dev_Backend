877f7fe32773f800a0ce17324dc50fe2836ad204
Matheus Souza david <140615573+Teuz1@users.noreply.github.com>
2024-01-02 09:30:03 -0300

    Update dev_backend_Repository.java

--- DevBackEnd/dev_backend_Repository.java
+++ DevBackEnd/dev_backend_Repository.java
@@ -1,5 +1,19 @@
-package DevBackEnd;
+\package DevBackEnd;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import DevBackEnd.dev_backend_Model.Usuario;
+
 
 public class dev_backend_Repository {
 
+	// UsuarioRepository.java
+	public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	    Optional<Usuario> findByEmail(String email);
+	    // Adicione métodos de consulta específicos, se necessário
+	}
+
+	
 }
